340d334990ecb4ef03f02b089c44a2cba081b0b5
yihuang <yi.codeplayer@gmail.com>
2012-08-15 18:27:57 +0800

    add threshold

--- Node.hs
+++ Node.hs
@@ -7,19 +7,22 @@ import Event
 data Status = Active | Inactive
 
 data Node a = Node
-  { status :: IORef Status
-  , state  :: IORef a
-  , event  :: Event a
+  { status    :: IORef Status
+  , state     :: IORef a
+  , event     :: Event a
+  , threshold :: a
   }
 
-newNode :: Status -> a -> IO (Node a)
-newNode status a = Node <$> newIORef (status, a) <*> newEvent
+newNode :: Status -> a -> a -> IO (Node a)
+newNode status a s = Node <$> newIORef status <*> newIORef a <*> newEvent <*> pure s
 
 fireNode :: Node a -> IO ()
 fireNode node = do
     a <- readIORef (state node)
     fire (event node) a
 
-connectNode :: Node a -> Node b -> (a -> b -> b) -> IO ()
+connectNode :: Ord b => Node a -> Node b -> (a -> b -> b) -> IO ()
 connectNode from to f = listen (event from) $ \a -> do
                           modifyIORef (state to) (f a)
+                          b <- readIORef (state to)
+                          writeIORef (status to) (if (b>=threshold to) then Active else Inactive)
